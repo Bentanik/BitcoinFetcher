35020f4209ce182aa8ddcaa29652b963b3dc6ee6
NguyenMaiVietVy <vietvyqw@gmail.com>
2024-10-04 10:08:54 +0700

    Add project files.

---
+++ BitCoinFetcherWPF/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Linq;
+using System.Timers;
+using System.Windows;
+
+namespace BitcoinPriceViewer
+{
+    public partial class MainWindow : Window
+    {
+        private static ConnectionMultiplexer _redis;
+        private static IDatabase _db;
+        private static System.Timers.Timer _timer;
+        private DateTime _startTime;
+        private double _startPrice;
+        private double _minPrice;
+        private double _maxPrice;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            _redis = ConnectionMultiplexer.Connect("localhost:6379,abortConnect=false,connectTimeout=10000");
+            if (!_redis.IsConnected)
+            {
+                return;
+            }
+            _db = _redis.GetDatabase();
+            _startTime = DateTime.Now;
+            FetchInitialPrice();
+            StartTimer();
+        }
+
+
+
+        private async void FetchInitialPrice()
+        {
+            var firstPrice = await GetFirstPrice();
+            _startPrice = firstPrice;
+            StartInfoText.Text = $"{_startTime} - {_startPrice}";
+        }
+
+        private void StartTimer()
+        {
+            _timer = new System.Timers.Timer(1000);
+            _timer.Elapsed += UpdateUI;
+            _timer.Start();
+        }
+
+        private async void UpdateUI(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(async () =>
+            {
+                CurrentTimeText.Text = DateTime.Now.ToString("o");
+                await UpdatePrices();
+            });
+        }
+
+        private async Task UpdatePrices()
+        {
+            if (_redis.IsConnected)
+            {
+                var prices = await _db.ListRangeAsync("bitcoin_prices", 0, -1);
+                var parsedPrices = prices.Select(p => ParsePrice(p)).ToList();
+
+                if (parsedPrices.Count > 0)
+                {
+                    double min = parsedPrices.Min(p => p.Price);
+                    double max = parsedPrices.Max(p => p.Price);
+
+                    MinInfoText.Text = $"{parsedPrices.First(p => p.Price == min).Time} - {min}";
+                    MaxInfoText.Text = $"{parsedPrices.First(p => p.Price == max).Time} - {max}";
+
+                    RecentPricesList.ItemsSource = parsedPrices.Select(p => $"{p.Time} - {p.Price}").
+                        Skip(Math.Max(0, parsedPrices.Count - 50));
+                }
+            }
+        }
+
+        private async Task<double> GetFirstPrice()
+        {
+            if (_redis.IsConnected)
+            {
+                var prices = await _db.ListRangeAsync("bitcoin_prices", 0, 0);
+                return double.Parse(prices.First().ToString().Split("|")[1]);
+            }
+            return 0;
+        }
+
+        private (DateTime Time, double Price) ParsePrice(string entry)
+        {
+            var parts = entry.Split("|");
+            return (DateTime.Parse(parts[0]), double.Parse(parts[1]));
+        }
+    }
+}
---
+++ BitcoinFetcherConsole/Program.cs
@@ -0,0 +1,68 @@
+﻿using StackExchange.Redis;
+using Microsoft.Extensions.Configuration;
+
+namespace BitcoinFetcherConsole;
+
+class Program
+{
+    private static IConfigurationRoot configuration;
+    private static string redisConnectionString;
+    private static string bitcoinApiUrl;
+    private static TimeSpan fetchInterval;
+
+    static async Task Main(string[] args)
+    {
+        LoadConfiguration();
+
+        using (var redis = ConnectionMultiplexer.Connect(redisConnectionString))
+        {
+            var db = redis.GetDatabase();
+
+            while (true)
+            {
+                try
+                {
+                    DateTime utcNow = DateTime.UtcNow;
+                    TimeZoneInfo vietnamTimeZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
+                    DateTime vietnamDateTime = TimeZoneInfo.ConvertTimeFromUtc(utcNow, vietnamTimeZone);
+                    var price = await FetchBitcoinPriceAsync();
+
+                    var message = $"{vietnamDateTime}|{price}";
+                    await db.ListRightPushAsync("bitcoin_prices", message);
+
+                    Console.WriteLine($"[{vietnamDateTime}] Price Bitcoin: {price} USD");
+                    await Task.Delay(fetchInterval);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+    }
+
+    private static void LoadConfiguration()
+    {
+        configuration = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        var _redisHost = configuration["Redis:Host"];
+        var _redisPort = int.Parse(configuration["Redis:Port"]);
+        redisConnectionString = $"{_redisHost}:{_redisPort}";
+        bitcoinApiUrl = configuration["BitcoinApi:Url"];
+        fetchInterval = TimeSpan.FromSeconds(int.Parse(configuration["AppSettings:FetchIntervalInSeconds"]));
+    }
+
+    private static async Task<decimal> FetchBitcoinPriceAsync()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            var response = await client.GetStringAsync(bitcoinApiUrl);
+            dynamic json = Newtonsoft.Json.JsonConvert.DeserializeObject(response);
+
+            return json.bpi.USD.rate_float;
+        }
+    }
+}
